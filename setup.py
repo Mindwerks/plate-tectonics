914d27fc3224518538bcf661721a8b3572f6421f
Federico Tomassetti <f.tomassetti@gmail.com>
2013-12-27 16:57:22 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup, Extension
+
+extra_compile_args = "-std=c99"
+
+pyplatec = Extension('platec',                    
+                    sources = ['platec_src/platecmodule.c',
+                        'platec_src/platecapi.cpp',
+                        'platec_src/plate.cpp',
+                        'platec_src/lithosphere.cpp',
+                        'platec_src/sqrdmd.c'],
+                    extra_compile_args=[extra_compile_args],
+                    )
+
+setup (name = 'PyPlatec',
+       version = '1.0',
+       author = "Federico Tomassetti",
+       author_email = "f.tomassetti@gmail.com",
+       url = "https://github.com/ftomassetti/pyplatec",
+       description = 'Plates simulation library',
+       ext_modules = [pyplatec])
