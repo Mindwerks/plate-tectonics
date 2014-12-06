0787d5055699823c6b14a05063082f2ffdbdaa4e
Federico Tomassetti <f.tomassetti@gmail.com>
2014-12-06 13:00:19 +0000

    Improving SetAndGetHeightMap

--- test/test_heightmap.cpp
+++ test/test_heightmap.cpp
@@ -4,6 +4,12 @@
 TEST(SetAndGetHeightMap)
 {
   HeightMap hm = HeightMap(50, 20);
+  hm.set( 0,  0, 0.2f);
+  hm.set(20, 18, 0.7f);
   hm.set(40, 18, 0.5f);
+  hm.set(49, 19, 0.9f);
+  CHECK(0.2f == hm.get( 0,  0));
+  CHECK(0.7f == hm.get(20, 18));
   CHECK(0.5f == hm.get(40, 18));
+  CHECK(0.9f == hm.get(49, 19));
 }
