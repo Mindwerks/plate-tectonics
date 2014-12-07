68c302e75bc65ff06ba43fe559e38e61a07a8c74
Federico Tomassetti <f.tomassetti@gmail.com>
2014-12-07 13:22:31 +0000

    Activating all warnings when compiling tests

--- test/compile.sh
+++ test/compile.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-g++ -c ../src/heightmap.cpp
-g++ -c ../src/plate.cpp
-g++ -c ../src/platecapi.cpp
-g++ -c ../src/lithosphere.cpp
-gcc -std=c99 -c ../src/sqrdmd.c
-g++ -c test_heightmap.cpp -I ../src
-g++ -c test_plate.cpp -I ../src
-g++ -c test_acceptance.cpp -I ../src
+export FLAGS="-Wall"
+g++ $FLAGS -c ../src/heightmap.cpp
+g++ $FLAGS -c ../src/plate.cpp
+g++ $FLAGS -c ../src/platecapi.cpp
+g++ $FLAGS -c ../src/lithosphere.cpp
+gcc $FLAGS -std=c99 -c ../src/sqrdmd.c
+g++ $FLAGS -c test_heightmap.cpp -I ../src
+g++ $FLAGS -c test_plate.cpp -I ../src
+g++ $FLAGS -c test_acceptance.cpp -I ../src
 g++ sqrdmd.o lithosphere.o plate.o test_plate.o heightmap.o test_heightmap.o platecapi.o test_acceptance.o -l UnitTest++ -o tests_runner
