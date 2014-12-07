497ba1ec91bd7cfe717ab12e1c0afb2375333450
Federico Tomassetti <f.tomassetti@gmail.com>
2014-12-07 09:27:53 +0000

    Using CHECKF_EQ iin PlatecCreate

--- test/test_acceptance.cpp
+++ test/test_acceptance.cpp
@@ -16,11 +16,11 @@ TEST(PlatecCreate)
   void* p = platec_api_create(512,0.65,60,0.02,1000000,0.33,2,10);
   const float* heightmap = platec_api_get_heightmap(p);
 
-  CHECK(1.8901190757751465f == heightmap[0]);
-  CHECK(1.9333564043045044f == heightmap[100]);
-  CHECK(0.10000000149011612f == heightmap[200]);
-  CHECK(0.10000000149011612f == heightmap[100000]);
-  CHECK(1.7803807258605957f == heightmap[262143]);
+  CHECKF_EQ(1.8901190757751465f,  heightmap[0]);
+  CHECKF_EQ(1.9333564043045044f,  heightmap[100]);
+  CHECKF_EQ(0.10000000149011612f, heightmap[200]);
+  CHECKF_EQ(0.10000000149011612f, heightmap[100000]);
+  CHECKF_EQ(1.7803807258605957f,  heightmap[262143]);
 }
 
 TEST(PlatecGlobalGeneration)
@@ -35,6 +35,6 @@ TEST(PlatecGlobalGeneration)
   CHECKF_EQ(0.13348780572414398f, heightmap[0]);
   CHECKF_EQ(0.21046529710292816f, heightmap[100]);
   CHECKF_EQ(0.17170283198356628f, heightmap[200]);
-  CHECKF_EQ(2.9937195777893066f, heightmap[100000]);
+  CHECKF_EQ(2.9937195777893066f,  heightmap[100000]);
   CHECKF_EQ(0.17201107740402222f, heightmap[262143]);
 }
