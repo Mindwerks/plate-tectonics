6fde3d95ccc63f462d0846b9504fdd2188d78a4b
Federico Tomassetti <f.tomassetti@gmail.com>
2014-12-31 19:02:08 +0100

    plate: add test NotSquareDoesNotExplode

--- test/test_plate.cpp
+++ test/test_plate.cpp
@@ -1,10 +1,16 @@
 #include "plate.hpp"
 #include "gtest/gtest.h"
 
-TEST(CreatePlate, DoesNotExplode)
+TEST(CreatePlate, SquareDoesNotExplode)
 {
   const float *heightmap = new float[40000]; // 200 x 200
   plate p = plate(heightmap, 100, 3, 50, 23, 18, WorldDimension(200, 200));
+}
+
+TEST(CreatePlate, NotSquareDoesNotExplode)
+{
+  const float *heightmap = new float[40000]; // 200 x 200
+  plate p = plate(heightmap, 100, 3, 50, 23, 18, WorldDimension(200, 400));
 }
 
 int main(int argc, char **argv) {
