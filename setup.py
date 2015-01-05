d26143c0264bdae476070574dd74a44f8f56844b
Federico Tomassetti <f.tomassetti@gmail.com>
2015-01-05 15:57:37 +0100

    Moving to version 1.2.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ pyplatec = Extension('platec',
                     )
 
 setup (name = 'PyPlatec',
-       version = '1.2',
+       version = '1.2.1',
        author = "Federico Tomassetti",
        author_email = "f.tomassetti@gmail.com",
        url = "https://github.com/ftomassetti/pyplatec",
