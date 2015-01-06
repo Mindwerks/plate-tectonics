fbcd6a753778675e9e72dfee12e383ed566cc555
Federico Tomassetti <f.tomassetti@gmail.com>
2015-01-06 12:03:08 +0100

    sqrdmd: fixing test

--- test/test_sqrdmd.cpp
+++ test/test_sqrdmd.cpp
@@ -5,13 +5,13 @@
 TEST(SqrDmd, DoNotExplodeWithWidthLargerThanHeight)
 {
     long seed = 3;
-    float map[128 * 32];
-    sqrdmd(seed, map, 128, 32, 0.5);
+    float map[129 * 33];
+    sqrdmd(seed, map, 129, 33, 0.5);
 }
 
 TEST(SqrDmd, DoNotExplodeWithHeightLargerThanWidth)
 {
     long seed = 3;
-    float map[32 * 128];
-    sqrdmd(seed, map, 32, 128, 0.5);
+    float map[33 * 129];
+    sqrdmd(seed, map, 33, 129, 0.5);
 }
