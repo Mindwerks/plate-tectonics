ebfa9f3fbc7f12114c52b5d4fc828ae0b49fd3e7
Federico Tomassetti <f.tomassetti@gmail.com>
2015-01-06 11:56:05 +0100

    sqrdmd: fixing tests to use dimensions == pow(2,n)+1

--- test/test_sqrdmd.cpp
+++ test/test_sqrdmd.cpp
@@ -5,13 +5,13 @@
 TEST(SqrDmd, DoNotExplodeWithWidthLargerThanHeight)
 {
     long seed = 3;
-    float map[100 * 20];
-    sqrdmd(seed, map, 100, 20, 0.5);
+    float map[128 * 32];
+    sqrdmd(seed, map, 128, 32, 0.5);
 }
 
 TEST(SqrDmd, DoNotExplodeWithHeightLargerThanWidth)
 {
     long seed = 3;
-    float map[20 * 100];
-    sqrdmd(seed, map, 20, 100, 0.5);
+    float map[32 * 128];
+    sqrdmd(seed, map, 32, 128, 0.5);
 }
