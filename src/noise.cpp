f469e482e9ef6d02fbd5bc4c0dc8c34b795ebec5
Federico Tomassetti <ftomassetti@groupon.com>
2015-03-03 14:30:59 +0000

    Re-enabling simplex noise

--- src/noise.cpp
+++ src/noise.cpp
@@ -20,7 +20,7 @@ static uint32_t nearest_pow(uint32_t num)
 void createNoise(float* tmp, const WorldDimension& tmpDim, SimpleRandom randsource, bool useSimplex)
 {
     // FIXME simplex has been disabled for now
-    useSimplex = false;
+    //useSimplex = false;
 try {
     if (useSimplex) {
         simplexnoise(randsource.next(), tmp, 
