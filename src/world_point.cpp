7259cb73c279f9df7d3a646e7204130c73c8abc8
Federico Tomassetti <federico@tomassetti.me>
2015-03-05 14:30:57 +0000

    Removing useless semicolon from world_point.cpp

--- src/world_point.cpp
+++ src/world_point.cpp
@@ -5,26 +5,26 @@ WorldPoint::WorldPoint(uint32_t x, uint32_t y)
     : _x(x),
       _y(y)
 {
-};
+}
 
 WorldPoint::WorldPoint(const WorldPoint& other)
     : _x(other.x()),
       _y(other.y())
 {
 
-};
+}
 
 uint32_t WorldPoint::x() const
 {
     return _x;
-};
+}
 
 uint32_t WorldPoint::y() const
 {
     return _y;
-};
+}
 
 uint32_t WorldPoint::toIndex(const WorldDimension& dim) const
 {
     return _y * dim.getWidth() + _x;
-};
+}
