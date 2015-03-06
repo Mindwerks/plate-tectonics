c80aa8043d1ab86a688ccb6cae2ca0e7837cfc85
Federico Tomassetti <federico@tomassetti.me>
2015-03-06 11:43:14 +0000

    Testing MassBuilder and Mass

--- test/test_mass.cpp
+++ test/test_mass.cpp
@@ -13,3 +13,30 @@ TEST(MassBuilder, ConstructorFromHeightmap)
     EXPECT_FLOAT_EQ(2.4114285714285715f, mb.build().getCx());
     EXPECT_FLOAT_EQ(0.6514285714285715f, mb.build().getCy());
 }
+
+TEST(MassBuilder, AddPoint)
+{
+    MassBuilder mb;
+    EXPECT_FLOAT_EQ(0.0f, mb.build().getMass());
+
+    mb.addPoint(10, 10, 123.0);
+    EXPECT_FLOAT_EQ(123.0f, mb.build().getMass());
+    EXPECT_FLOAT_EQ(10.0f, mb.build().getCx());
+    EXPECT_FLOAT_EQ(10.0f, mb.build().getCy());
+
+    mb.addPoint(0, 5, 123.0);
+    EXPECT_FLOAT_EQ(246.0f, mb.build().getMass());
+    EXPECT_FLOAT_EQ(5.0f, mb.build().getCx());
+    EXPECT_FLOAT_EQ(7.5f, mb.build().getCy());
+}
+
+TEST(Mass, Constructor)
+{
+    Mass mass1(0.0f, 7.5f, 8.5f);
+    EXPECT_FLOAT_EQ(0.0f, mass1.getMass());
+
+    Mass mass2(8.5f, 7.6f, 27.5f);
+    EXPECT_FLOAT_EQ(8.5f, mass2.getMass());
+    EXPECT_FLOAT_EQ(7.6f, mass2.getCx());
+    EXPECT_FLOAT_EQ(27.5f, mass2.getCy());
+}
