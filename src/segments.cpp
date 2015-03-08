8108d05b37a57d8c5238eb29d5048d6c08306223
Federico Tomassetti <federico@tomassetti.me>
2015-03-08 14:20:56 +0000

    Moving implementaton of Segments from hpp to cpp file

--- src/segments.cpp
+++ src/segments.cpp
@@ -1,1 +1,81 @@
 #include "segments.hpp"
+
+Segments::Segments(uint32_t plate_area)
+{
+    _area = plate_area;
+    segment = new uint32_t[plate_area];
+    memset(segment, 255, plate_area * sizeof(uint32_t));
+}
+
+Segments::~Segments()
+{   
+    delete[] segment;
+    segment = NULL;
+    _area = 0;
+}
+
+uint32_t Segments::area()
+{
+    return _area;
+}
+
+void Segments::reset()
+{
+    memset(segment, -1, sizeof(uint32_t) * _area);
+    seg_data.clear();
+}
+
+void Segments::reassign(uint32_t newarea, uint32_t* tmps)
+{
+    delete[] segment;
+    _area = newarea;
+    segment = tmps;
+}
+
+void Segments::shift(uint32_t d_lft, uint32_t d_top)
+{
+    for (uint32_t s = 0; s < seg_data.size(); ++s)
+    {
+        seg_data[s].shift(d_lft, d_top);
+    }
+}
+
+uint32_t Segments::size() const
+{
+    return seg_data.size();
+}
+
+const SegmentData& Segments::operator[](uint32_t index) const
+{
+    return seg_data[index];
+}
+
+SegmentData& Segments::operator[](uint32_t index)
+{
+    return seg_data[index];
+}
+
+void Segments::add(const SegmentData& data){
+    seg_data.push_back(data);
+}
+
+const ContinentId& Segments::id(uint32_t index) const {
+    if (index>=_area) {
+        throw runtime_error("unvalid index");
+    }
+    return segment[index];
+}
+
+ContinentId& Segments::id(uint32_t index) {
+    if (index>=_area) {
+        throw runtime_error("unvalid index");
+    }
+    return segment[index];
+}
+
+void Segments::setId(uint32_t index, ContinentId id) const {
+    if (index>=_area) {
+        throw runtime_error("unvalid index");
+    }
+    segment[index] = id;
+}
--- src/segments.hpp
+++ src/segments.hpp
@@ -17,76 +17,19 @@ typedef uint32_t ContinentId;
 class Segments
 {
 public:
-	Segments(uint32_t plate_area)
-	{
-		_area = plate_area;
-    	segment = new uint32_t[plate_area];
-    	memset(segment, 255, plate_area * sizeof(uint32_t));
-	}
-	~Segments()
-	{	
-		delete[] segment;
-    	segment = NULL;
-    	_area = 0;
-	}
-	uint32_t area()
-	{
-		return _area;
-	}
-	void reset()
-	{
-		memset(segment, -1, sizeof(uint32_t) * _area);
-    	seg_data.clear();
-	}
-	void reassign(uint32_t newarea, uint32_t* tmps)
-	{
-		delete[] segment;
-        _area = newarea;
-        segment = tmps;
-	}
-	void shift(uint32_t d_lft, uint32_t d_top)
-	{
-		for (uint32_t s = 0; s < seg_data.size(); ++s)
-        {
-            seg_data[s].shift(d_lft, d_top);
-        }
-	}
-	uint32_t size() const
-	{
-		return seg_data.size();
-	}
-	const SegmentData& operator[](uint32_t index) const
-	{
-		return seg_data[index];
-	}
-	SegmentData& operator[](uint32_t index)
-	{
-		return seg_data[index];
-	}
-	void add(const SegmentData& data){
-		seg_data.push_back(data);
-	}
-	const ContinentId& id(uint32_t index) const {
-		if (index>=_area) {
-			throw runtime_error("unvalid index");
-		}
-		return segment[index];
-	}
-	ContinentId& id(uint32_t index) {
-		if (index>=_area) {
-			throw runtime_error("unvalid index");
-		}
-		return segment[index];
-	}
-	void setId(uint32_t index, ContinentId id) const {
-		if (index>=_area) {
-			throw runtime_error("unvalid index");
-		}
-		/*if (id>=seg_data.size()){
-			throw runtime_error("unvalid id");	
-		}*/
-		segment[index] = id;
-	}
+	Segments(uint32_t plate_area);
+	~Segments();
+	uint32_t area();
+	void reset();
+	void reassign(uint32_t newarea, uint32_t* tmps);
+	void shift(uint32_t d_lft, uint32_t d_top);
+	uint32_t size() const;
+	const SegmentData& operator[](uint32_t index) const;
+	SegmentData& operator[](uint32_t index);
+	void add(const SegmentData& data);
+	const ContinentId& id(uint32_t index) const;
+	ContinentId& id(uint32_t index);
+	void setId(uint32_t index, ContinentId id) const;
 private:
 	std::vector<SegmentData> seg_data; ///< Details of each crust segment.
 	ContinentId* segment;              ///< Segment ID of each piece of continental crust.
