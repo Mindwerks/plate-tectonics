93f6f69650f7086fbd1b05934274e7549cab557d
Federico Tomassetti <federico@tomassetti.me>
2015-03-11 14:10:02 +0000

    Documenting IBounds

--- src/bounds.hpp
+++ src/bounds.hpp
@@ -112,54 +112,18 @@ public:
            const Dimension& dimension);
 
     uint32_t index(uint32_t x, uint32_t y) const;
-
     uint32_t area() const;
-
     uint32_t width() const;
-
     uint32_t height() const;
-
     uint32_t leftAsUint() const;
-
     uint32_t topAsUint() const;
-
     uint32_t rightAsUintNonInclusive() const;
-
     uint32_t bottomAsUintNonInclusive() const;
-
-    /// Given a point in World relative coordinates, it tells if it is part of the plate or not.
     bool containsWorldPoint(uint32_t x, uint32_t y) const;
-
-    /// Given a point in plate relative coordinates, it tells if it is part of the plate or not.
     bool isInLimits(float x, float y) const;
-
-    /// Shift the position of the top left corner by the given amount.
-    /// It preserves the dimension of the plate.
     void shift(float dx, float dy);
-
     void grow(int dx, int dy);
-
-    /// Translate world coordinates into offset within plate's height map.
-    ///
-    /// If the global world map coordinates are within plate's height map,
-    /// the values of passed coordinates will be altered to contain the
-    /// X and y offset within the plate's height map. Otherwise an exception is thrown.
-    ///
-    /// @param[in, out] x   Offset on the global world map along X axis.
-    /// @param[in, out] y   Offset on the global world map along Y axis.
-    /// @return             Offset in height map or BAD_INDEX on error.
     uint32_t getValidMapIndex(uint32_t* px, uint32_t* py) const;
-
-    /// Translate world coordinates into offset within plate's height map.
-    ///
-    /// Iff the global world map coordinates are within plate's height map,
-    /// the values of passed coordinates will be altered to contain the
-    /// X and y offset within the plate's height map. Otherwise values are
-    /// left intact.
-    ///
-    /// @param[in, out] x   Offset on the global world map along X axis.
-    /// @param[in, out] y   Offset on the global world map along Y axis.
-    /// @return             Offset in height map
     uint32_t getMapIndex(uint32_t* x, uint32_t* y) const;
     
 private:
