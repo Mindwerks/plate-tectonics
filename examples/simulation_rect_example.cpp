c116b3c3958fa23536bbd4906fea51ea0050be51
Federico Tomassetti <federico@tomassetti.me>
2015-03-12 20:32:53 +0000

    Adding the possibilities to specify several parameters

--- examples/simulation_rect_example.cpp
+++ examples/simulation_rect_example.cpp
@@ -10,23 +10,9 @@
 #include <unistd.h>
 #include <cstdlib>
 #include <iostream>
-#include <stdexcept>
+#include <utils.hpp>
 
 #include <execinfo.h>
-
-
-void handler() {
-  printf("\nException caused failure\n\n");
-  void *trace_elems[20];
-    int trace_elem_count(backtrace( trace_elems, 20 ));
-    char **stack_syms(backtrace_symbols( trace_elems, trace_elem_count ));
-    for ( int i = 0 ; i < trace_elem_count ; ++i )
-    {
-        std::cout << stack_syms[i] << "\n";
-    }
-    free( stack_syms );
-  exit(1);
-}
 
 void produce_image(float* heightmap, int width, int height, const char* filename)
 {
@@ -49,16 +35,122 @@ void save_image(void* p, const char* filename, const int width, const int height
     delete copy;
 }
 
-int main(int argc, char* argv[])
+typedef struct {
+    uint32_t seed;
+    uint32_t width;
+    uint32_t height;
+    bool colors;
+    char* filename;
+    uint32_t step;
+} Params;
+
+char DEFAULT_FILENAME[] = "simulation";
+
+void fill_params(Params& params, int argc, char* argv[])
 {
-    std::set_terminate( handler );
+    srand(time(NULL));
+    params.seed = rand();
+    params.width = 600;
+    params.height = 400;
+    params.colors = true;
+    params.filename = DEFAULT_FILENAME;
+    params.step = 0;
 
-    int seed = 10;
-    int width = 800;
-    int height = 600;
-    printf("Generating a map with seed %d\n", seed);
+    int p = 1;
+    while (p < argc) {
+        if (0 == strcmp(argv[p], "--help") || 0 == strcmp(argv[p], "-h")){
+            printf(" -h --help           : show this message\n");
+            printf(" -s SEED             : use the given SEED\n");
+            printf(" --dim WIDTH HEIGHT  : use the given width and height\n");
+            printf(" --colors            : generate a colors map\n");
+            printf(" --grayscale         : generate a grayscale map\n");
+            printf(" --filename FILENAME : generated map are named with the given filename (the extension is appended)\n");
+            printf(" --step X            : generate intermediate maps any given steps\n");
+            exit(0);
+        } else if (0 == strcmp(argv[p], "-s")) {
+            if (p + 1 >= argc){
+                printf("error: a parameter should follow -s\n");
+                exit(1);
+            }
+            p += 2;
+        } else if (0 == strcmp(argv[p], "--dim")) {
+            if (p + 2 >= argc){
+                printf("error: two parameters should follow --dim\n");
+                exit(1);
+            }
+            int width = atoi(argv[p+1]);
+            int height = atoi(argv[p+2]);
+            if (width==0 || height==0){
+                printf("error: not a number\n");
+                exit(1);   
+            }
+            if (width<0 || height<0){
+                printf("error: dimensions have to be positive\n");
+                exit(1);   
+            }
+            params.width = width;
+            params.height = height;
+            p += 3;
+        } else if (0 == strcmp(argv[p], "--colors")) {
+            params.colors = true;
+            p += 1;
+        } else if (0 == strcmp(argv[p], "--grayscale")) {
+            params.colors = false;
+            p += 1;
+        } else if (0 == strcmp(argv[p], "--filename")) {            
+            if (p + 1 >= argc){
+                printf("error: a parameter should follow --filename\n");
+                exit(1);
+            }
+            p += 2;
+        } else if (0 == strcmp(argv[p], "--step")) {
+            if (p + 1 >= argc){
+                printf("error: a parameter should follow --step\n");
+                exit(1);
+            }
+            int step = atoi(argv[p+1]);
+            if (step==0){
+                printf("error: not a number\n");
+                exit(1);   
+            }
+            if (step<0){
+                printf("error: step have to be positive\n");
+                exit(1);   
+            }            
+            params.step = step;
+            p += 2;
+        } else {
+            printf("Unexpected param '%s' use -h to display a list of params\n", argv[p]);
+            exit(1);
+        }
+    }
+}
 
-    void* p = platec_api_create(seed, width, height, 0.65, 60, 0.02,1000000, 0.33, 2, 10);
+/// Should take several parameters:
+/// - colors/grayscale
+/// - width and height
+/// - if to generate the intermediate imaged and how frequently
+/// - the output filename
+/// - the seed
+int main(int argc, char* argv[])
+{    
+    Params params;
+    fill_params(params, argc, argv);
+
+    printf("Plate-tectonics simulation example");
+    printf(" seed     : %d\n", params.seed);
+    printf(" width    : %d\n", params.width);
+    printf(" height   : %d\n", params.height);
+    printf(" map      : %s\n", params.colors ? "colors" : "grayscale");
+    printf(" filename : %s\n", params.filename);
+    if (params.step == 0)
+        printf(" step     : no\n");
+    else
+        printf(" step     : %i\n", params.step);
+
+    printf("\n");
+
+    void* p = platec_api_create(params.seed, params.width, params.height, 0.65, 60, 0.02,1000000, 0.33, 2, 10);
     printf(" * initial map created\n");
 
     int step = 0;
@@ -66,14 +158,16 @@ int main(int argc, char* argv[])
         step++;
         platec_api_step(p);
 
-        if (step % 100 == 0) {
-            printf(" * step %i\n", step);
-            char filename[50];
-            sprintf(filename, "simulation_rect_%i.png", step);
-            save_image(p, filename, width, height);
+        if (params.step != 0 && (step % params.step == 0) ) {            
+            char filename[250];
+            sprintf(filename, "%s_%i.png", params.filename, step);
+            printf(" * step %i (filename %s)\n", step, filename);
+            save_image(p, filename, params.width, params.height);
         }
     }
-
-    printf(" * simulation completed\n");
-    save_image(p, "simulation_rect.png", width, height);
+    
+    char filename[250];
+    sprintf(filename, "%s.png", params.filename);
+    save_image(p, filename, params.width, params.height);
+    printf(" * simulation completed (filename %s)\n", filename);
 }
