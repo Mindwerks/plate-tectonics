4a7a1f8867db1503180644895ae7c4171d09052d
Federico Tomassetti <federico@tomassetti.me>
2015-03-22 17:00:54 +0000

    Initial skeleton for tests

---
+++ test/test_generation.py
@@ -0,0 +1,26 @@
+import unittest
+from worldengine.common import *
+
+
+class TestGeneration(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_create(self):
+        pass
+
+    def test_destroy(self):
+        pass
+
+    def test_get_heightmap(self):
+        pass
+
+    def test_get_platesmap(self):
+        pass
+
+    def test_step(self):
+        pass
+
+    def test_is_finished(self):
+        pass
