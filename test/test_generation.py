7b83fdcc1a5d308e1284488d8b7765501252d83d
Federico Tomassetti <federico@tomassetti.me>
2015-03-25 21:26:01 +0000

    Revising tests to not expect exactly same results across platforms

--- test/test_generation.py
+++ test/test_generation.py
@@ -20,17 +20,9 @@ class TestGeneration(unittest.TestCase):
         p = platec.create(seed, width, height, 0.65, 60, 0.02, 1000000, 0.33, 2, 10)
         hm = platec.get_heightmap(p)
         platec.destroy(p)
-        self.assertAlmostEqual(0.10000000149011612, hm[0])
-        self.assertAlmostEqual(0.10000000149011612, hm[1000])
-        self.assertAlmostEqual(0.10000000149011612, hm[2000])
-        self.assertAlmostEqual(1.7753722667694092,  hm[3000])
-        self.assertAlmostEqual(0.10000000149011612, hm[4000])
-        self.assertAlmostEqual(0.10000000149011612, hm[5000])
-        self.assertAlmostEqual(0.10000000149011612, hm[6000])
-        self.assertAlmostEqual(0.10000000149011612, hm[7000])
-        self.assertAlmostEqual(1.7841405868530273, hm[8000])
-        self.assertAlmostEqual(0.10000000149011612, hm[9000])
-        self.assertAlmostEqual(0.10000000149011612, hm[9999])
+        self.assertEqual(10000, len(hm))
+        for v in hm:
+            self.assertTrue(v >= 0.0)
 
     def test_get_platesmap(self):
         seed = 1
@@ -39,17 +31,9 @@ class TestGeneration(unittest.TestCase):
         p = platec.create(seed, width, height, 0.65, 60, 0.02, 1000000, 0.33, 2, 10)
         pm = platec.get_platesmap(p)
         platec.destroy(p)
-        self.assertEqual(2, pm[0])
-        self.assertEqual(2, pm[1000])
-        self.assertEqual(0, pm[2000])
-        self.assertEqual(9, pm[3000])
-        self.assertEqual(9, pm[4000])
-        self.assertEqual(9, pm[5000])
-        self.assertEqual(6, pm[6000])
-        self.assertEqual(6, pm[7000])
-        self.assertEqual(6, pm[8000])
-        self.assertEqual(2, pm[9000])
-        self.assertEqual(2, pm[9999])
+        self.assertEqual(10000, len(pm))
+        for v in pm:
+            self.assertTrue(10 > v >= 0)
 
     def test_step(self):
         pass
