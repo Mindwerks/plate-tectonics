3b174e7bca1889aef5ea880fdaf1c2bc2270a558
Federico Tomassetti <ftomassetti@groupon.com>
2015-05-06 09:16:33 +0100

    travis: export PYCMD and PIP for py26 from main Travis script

--- travis/prepare_py26.sh
+++ travis/prepare_py26.sh
@@ -3,5 +3,3 @@ sudo apt-get install python2.6 python2.6-dev
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O ez_setup.py
 sudo python2.6 ez_setup.py
 sudo easy_install-2.6 pip
-export PYCMD="python2.6"
-export PIP="pip2.6"
