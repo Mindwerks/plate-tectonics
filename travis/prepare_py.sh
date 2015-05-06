9f6cf89bc3b22e9ae97e779f7b5a02d677a90581
Federico Tomassetti <ftomassetti@groupon.com>
2015-05-06 10:57:04 +0100

    travis: moving commands to prepare_py.sh

---
+++ travis/prepare_py.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/yyuu/pyenv.git pyenv
+export PYENV_ROOT="pyenv"
+export PATH="$PYENV_ROOT/bin":"$PATH"
+then eval "$(pyenv init -)"
+pyenv install $PYVERSION
+pyenv local $PYVERSION
+export PYCMD="python"
+export PIP="pip"
