b0f8cc09882d0183402b42263a8f3f8339ce6350
Federico Tomassetti <ftomassetti@groupon.com>
2015-05-06 11:04:05 +0100

    travis: remove spurious 'then' in prepare_py.sh

--- travis/prepare_py.sh
+++ travis/prepare_py.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/yyuu/pyenv.git pyenv
 export PYENV_ROOT="pyenv"
 export PATH="$PYENV_ROOT/bin":"$PATH"
-then eval "$(pyenv init -)"
+eval "$(pyenv init -)"
 pyenv install $PYVERSION
 pyenv local $PYVERSION
 export PYCMD="python"
