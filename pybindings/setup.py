5be8bc58ee8cf31ece5a53ccbd8b408e7f8b54d3
Federico Tomassetti <ftomassetti@groupon.com>
2015-05-07 10:35:21 +0100

    ensuring cpp code is included in the src dist package

--- pybindings/setup.py
+++ pybindings/setup.py
@@ -1,23 +1,40 @@
 from setuptools import setup, Extension, Command
 import os
+import shutil
 
+def ensure_clean_dir(f):
+  if os.path.exists(f):
+    shutil.rmtree(f)
+  os.makedirs(f)
 
-current_dir = os.path.dirname(__file__)
-parent_dir = os.path.join(current_dir, os.pardir)
-cpp_src_dir = os.path.abspath(os.path.join(parent_dir, "src"))
+def copy_dir_contents(src, dst):
+  src_files = os.listdir(src)
+  for file_name in src_files:
+      full_file_name = os.path.join(src, file_name)
+      if (os.path.isfile(full_file_name)):
+          shutil.copy(full_file_name, dst)
 
+# If we are compiling from inside the tree we need to move the C++ source code
+# to cpp_src, otherwise if building from a source directory the C++ source code
+# should be already there
+cpp_src_dir = "cpp_src"
+
+if os.path.exists("../src"):
+  ensure_clean_dir(cpp_src_dir)
+  copy_dir_contents("../src", cpp_src_dir)
+
+# We add all .cpp files to the sources 
 sources = [ 'platec_src/platecmodule.cpp']
 for f in os.listdir(cpp_src_dir):
   if f.endswith(".cpp"):
     sources.append("%s/%s" % (cpp_src_dir, f))
-print(sources)
 
 pyplatec = Extension('platec',                    
                      sources = sources,
                      language='c++')
 
 setup (name = 'PyPlatec',
-       version = '1.3.1',
+       version = '1.3.2',
        author = "Federico Tomassetti",
        author_email = "f.tomassetti@gmail.com",
        url = "https://github.com/Mindwerks/pyplatec",
@@ -39,4 +56,5 @@ setup (name = 'PyPlatec',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ])
+      ]
+)
