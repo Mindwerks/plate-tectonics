c1b8f6f3ef61de845112d2424ef7080ae6821585
Federico Tomassetti <federico@tomassetti.me>
2015-05-07 19:01:47 +0100

    travis: ensure PyPlatec is removed before installing it

--- travis/build_and_test_sdist.sh
+++ travis/build_and_test_sdist.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sudo $PYCMD setup.py sdist
+# we ensure PyPlatec is removed (if not installed it would fail but that is ok)
+sudo $PIP uninstall PyPlatec -y -q || echo "ok"
 sudo $PIP install `ls dist/PyPlatec*.tar.gz`
