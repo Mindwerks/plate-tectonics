b3d2e139877fb746e19494fa15454422b68e23da
Bret Curtis <psi29a@gmail.com>
2025-03-31 22:04:04 +0200

    cleanup

--- examples/map_drawing.cpp
+++ examples/map_drawing.cpp
@@ -29,15 +29,15 @@ int writeImage(const char* filename, int width, int height, float *heightmap, co
 
     // Open file for writing (binary mode)
     fp = fopen(filename, "wb");
-    if (fp == NULL) {
+    if (fp == nullptr) {
         fprintf(stderr, "Could not open file %s for writing\n", filename);
         code = 1;
         goto finalise;
     }
 
     // Initialize write structure
-    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-    if (png_ptr == NULL) {
+    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+    if (png_ptr == nullptr) {
         fprintf(stderr, "Could not allocate write struct\n");
         code = 1;
         goto finalise;
@@ -45,7 +45,7 @@ int writeImage(const char* filename, int width, int height, float *heightmap, co
 
     // Initialize info structure
     info_ptr = png_create_info_struct(png_ptr);
-    if (info_ptr == NULL) {
+    if (info_ptr == nullptr) {
         fprintf(stderr, "Could not allocate info struct\n");
         code = 1;
         goto finalise;
@@ -60,13 +60,13 @@ int writeImage(const char* filename, int width, int height, float *heightmap, co
 
     png_init_io(png_ptr, fp);
 
-    // Write header (8 bit colour depth)
+    // Write header (8-bit colour depth)
     png_set_IHDR(png_ptr, info_ptr, width, height,
                  8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
                  PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
 
     // Set title
-    if (title != NULL) {
+    if (title != nullptr) {
         png_text title_text;
         title_text.compression = PNG_TEXT_COMPRESSION_NONE;
         title_text.key = "Title";
@@ -83,13 +83,13 @@ int writeImage(const char* filename, int width, int height, float *heightmap, co
     drawFunction(png_ptr, row, width, height, heightmap);
 
     // End write
-    png_write_end(png_ptr, NULL);
+    png_write_end(png_ptr, nullptr);
 
 finalise:
-    if (fp != NULL) fclose(fp);
-    if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
-    if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
-    if (row != NULL) free(row);
+    if (fp != nullptr) fclose(fp);
+    if (info_ptr != nullptr) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
+    if (png_ptr != nullptr) png_destroy_write_struct(&png_ptr, (png_infopp)nullptr);
+    if (row != nullptr) free(row);
 
     return code;
 }
