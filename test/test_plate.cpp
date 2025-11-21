96b9183fedc8e5649e0d587477eea81a9fb32bbc
Bret Curtis <psi29a@gmail.com>
2025-11-21 17:25:40 +0100

    fix issues related to platform differences

--- test/test_plate.cpp
+++ test/test_plate.cpp
@@ -47,11 +47,13 @@ TEST(SimpleRandom, NextRepeatability)
 
 TEST(Noise, SimplexRawNoiseRepeatability)
 {
-    EXPECT_FLOAT_EQ(-0.12851511f, raw_noise_4d(0.3f, 0.78f, 1.677f, 0.99f));
-    EXPECT_FLOAT_EQ(-0.83697641f, raw_noise_4d(-0.3f, 0.78f, 1.677f, 0.99f));
-    EXPECT_FLOAT_EQ(-0.5346415f, raw_noise_4d(7339.3f, 0.78f, 1.677f, 0.99f));
-    EXPECT_FLOAT_EQ(0.089452535f, raw_noise_4d(0.3f, 70.78f, 1.677f, 0.0009f));
-    EXPECT_FLOAT_EQ(-0.063593678f, raw_noise_4d(0.3f, 500.78f, 1.677f, 500.99f));
+    // Use EXPECT_NEAR with tolerance for cross-platform SIMD differences
+    const float tolerance = 0.0002f;
+    EXPECT_NEAR(-0.12851511f, raw_noise_4d(0.3f, 0.78f, 1.677f, 0.99f), tolerance);
+    EXPECT_NEAR(-0.83697641f, raw_noise_4d(-0.3f, 0.78f, 1.677f, 0.99f), tolerance);
+    EXPECT_NEAR(-0.5346415f, raw_noise_4d(7339.3f, 0.78f, 1.677f, 0.99f), tolerance);
+    EXPECT_NEAR(0.089452535f, raw_noise_4d(0.3f, 70.78f, 1.677f, 0.0009f), tolerance);
+    EXPECT_NEAR(-0.063593678f, raw_noise_4d(0.3f, 500.78f, 1.677f, 500.99f), tolerance);
 }
 
 TEST(Noise, SimplexNoiseRepeatability)
@@ -60,13 +62,15 @@ TEST(Noise, SimplexNoiseRepeatability)
     float *heightmap = new float[wd.getArea()];
     initializeHeightmapWithNoise(123, heightmap, wd);
 
-    EXPECT_FLOAT_EQ(0.50098729f, heightmap[0]);
-    EXPECT_FLOAT_EQ(0.39222634f, heightmap[1000]);
-    EXPECT_FLOAT_EQ(0.51659518f, heightmap[2000]);
-    EXPECT_FLOAT_EQ(0.5479334f, heightmap[5000]);
-    EXPECT_FLOAT_EQ(0.59222502f, heightmap[8000]);
-    EXPECT_FLOAT_EQ(0.36362505f, heightmap[11000]);
-    EXPECT_FLOAT_EQ(0.57599854f, heightmap[13000]);
+    // Use EXPECT_NEAR with tolerance for cross-platform SIMD differences
+    const float tolerance = 0.0002f;
+    EXPECT_NEAR(0.50098729f, heightmap[0], tolerance);
+    EXPECT_NEAR(0.39222634f, heightmap[1000], tolerance);
+    EXPECT_NEAR(0.51659518f, heightmap[2000], tolerance);
+    EXPECT_NEAR(0.5479334f, heightmap[5000], tolerance);
+    EXPECT_NEAR(0.59222502f, heightmap[8000], tolerance);
+    EXPECT_NEAR(0.36362505f, heightmap[11000], tolerance);
+    EXPECT_NEAR(0.57599854f, heightmap[13000], tolerance);
 
     delete[] heightmap;
 }
