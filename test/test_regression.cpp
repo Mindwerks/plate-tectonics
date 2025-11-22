fce373a77f7c12e54e85de140fb472cc16aeabad
Bret Curtis <psi29a@gmail.com>
2025-11-22 15:38:06 +0100

    unset msvc minmax that conflicts with std::min std::max

--- test/test_regression.cpp
+++ test/test_regression.cpp
@@ -17,6 +17,11 @@
  *  License along with this library; if not, see http://www.gnu.org/licenses/
  *****************************************************************************/
 
+// Prevent Windows.h from defining min/max macros that conflict with std::min/std::max
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
+
 #include "platecapi.hpp"
 #include "gtest/gtest.h"
 #include <cstdlib>
